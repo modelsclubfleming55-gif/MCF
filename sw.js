8584d78031f3aab98ccdb55a60083ccc4e07e982
modelsclubfleming55-gif <models.club.fleming55@gmail.com>
2026-05-29 10:09:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nom du cache (incrémentez v1, v2, etc. lors de modifications majeures)
-const CACHE_NAME = 'aero-club-v1';
+const CACHE_NAME = 'aero-club-v2';
 
 // Liste des fichiers à mettre en cache pour le mode hors-ligne
 const ASSETS_TO_CACHE = [
