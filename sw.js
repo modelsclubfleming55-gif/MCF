014052a6011b521a114a4e8965a91474b92198d1
modelsclubfleming55-gif <models.club.fleming55@gmail.com>
2026-06-30 23:24:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nom du cache (incrémentez v1, v2, etc. lors de modifications majeures)
-const CACHE_NAME = 'aero-club-v7';
+const CACHE_NAME = 'aero-club-v8';
 
 // Liste des fichiers à mettre en cache pour le mode hors-ligne
 const ASSETS_TO_CACHE = [
@@ -18,12 +18,11 @@ const ASSETS_TO_CACHE = [
     './images/interclub.jpg',
     './images/airdoudou.jpg',
     './images/terrainvol.jpg',   
-    './documents/infos Club Mai 2026.pdf',
     './documents/juin 2026.pdf',
     './documents/juillet 2026.pdf',
-    './documents/RÈGLEMENT INTÉRIEUR – Model’s Club Fleming (2026) .pdf'
-     './documents/air doudou 2026.pdf',
-    './documents/brocante 2026.pdf'
+    './documents/reglement2026.pdf',
+     './documents/air doudou 2026.pdf'
+
 ];
 
 // 1. Événement d'INSTALLATION : On stocke les fichiers
